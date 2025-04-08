7953a07f145e8bef35802879b0a69852bcd290b0
Erik <180784081+erhetrada@users.noreply.github.com>
2025-04-08 04:43:55 -0400

    initialize function to setup api button

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+function setupApiButton() {
+    const apiButton = document.getElementById('api-button');
+
+    apiButton.addEventListener('click', () => {
+        console.log('click');
+    });
+}
+
+setupApiButton();

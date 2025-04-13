f578a56adc1afc23045b538a839504c52adbba08
Erik <180784081+erhetrada@users.noreply.github.com>
2025-04-13 12:40:55 -0400

    change results endpoint message

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ db.run('CREATE TABLE IF NOT EXISTS "2024_presidential_general_election" (id INTE
 
 app.get('/results', (req, res) => {
   console.log('results endpoint hit');
-  res.json({ message: 'Success!' });
+  res.json({ message: 'results endpoint hit' });
 });
 
 // Start server

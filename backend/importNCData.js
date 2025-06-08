e6f885ab230b075a5a3c242f98edbebc21015739
Erik <180784081+erhetrada@users.noreply.github.com>
2025-06-07 23:38:07 -0400

    make rows from raw data

--- backend/importNCData.js
+++ backend/importNCData.js
@@ -5,6 +5,13 @@ const ELECTION_DATA_FILE_PATH = './NC_results_pct_20241105.txt'
 
 function parseElectionData(electionDataFilePath) {
     const rawElectionData = fs.readFileSync(electionDataFilePath, 'utf8');
+
+    // split data by line, filter out empty rows, split string rows into arrays
+    const rows = rawElectionData.split('\n').filter(Boolean).map(row => row.split('\t'));
+    console.log(rows[1]);
+
+      //const rows = raw.split('\n').filter(Boolean).map(row => row.split('\t'));
+
 }
 
 // Main control flow: parse, prepare, insert

e149364d146e011bce9e69abc7e5764407fcb700
Erik <180784081+erhetrada@users.noreply.github.com>
2025-06-13 02:39:51 -0400

    give ids integer type values

--- backend/importNCData.js
+++ backend/importNCData.js
@@ -35,11 +35,12 @@ const tableSchemas = {
   },
   voting_methods: {
     vote_id: { type: 'INTEGER', primaryKey: true },
-    election_id: {},
-    contest_id: {},
-    county_id: {},
-    precinct_id: {},
-    candidate_idmethod_id: {},
+    election_id: {type: 'INTEGER', foreignKey: ''},
+    contest_id: {type: 'INTEGER', foreignKey: ''},
+    county_id: {type: 'INTEGER', foreignKey: ''},
+    precinct_id: {type: 'INTEGER', foreignKey: ''},
+    candidate_id: {type: 'INTEGER', foreignKey: ''},
+    method_id: {type: 'INTEGER', foreignKey: ''},
     vote_count: { type: 'INTEGER' }
   }
 };

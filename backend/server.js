2a63767c1403110014205266f8179946e42a81b1
Erik <180784081+erhetrada@users.noreply.github.com>
2025-06-21 13:41:00 -0400

    rename error to err

--- backend/server.js
+++ backend/server.js
@@ -277,7 +277,7 @@ app.get('/:state/candidates', (req, res) => {
 // contests
 app.get('/:state/contests', (req, res) => {
   console.log('state-contests endpoint hit')
-  db.all('SELECT contest_name from nc_contests', (error, rows) => {
+  db.all('SELECT contest_name from nc_contests', (err, rows) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json(rows);
   });

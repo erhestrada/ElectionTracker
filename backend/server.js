d50c8258d616cf5fa474f3621aa1f982ebf51b76
Erik <180784081+erhetrada@users.noreply.github.com>
2025-06-22 19:04:42 -0400

    Join to nc_contests table and respond with contest_name instead of contest_id

--- backend/server.js
+++ backend/server.js
@@ -329,10 +329,11 @@ app.get('/:state/election-day-votes', (req, res) => {
   const offset = parseInt(req.query.offset) || 0;
 
   db.all(
-    `SELECT vote.vote_id, vote.election_id, vote.contest_id, county.county_name, vote.precinct_id, candidate.name, vote.method, vote.vote_count 
+    `SELECT vote.vote_id, vote.election_id, contest.contest_name, county.county_name, vote.precinct_id, candidate.name, vote.method, vote.vote_count 
      FROM nc_voting_methods vote
      JOIN nc_candidates candidate ON vote.candidate_id = candidate.candidate_id
      JOIN nc_counties county on vote.county_id = county.county_id 
+     JOIN nc_contests contest on vote.contest_id = contest.contest_id 
      WHERE method = ? 
      LIMIT ? OFFSET ?`,
     ['Election Day', limit, offset],

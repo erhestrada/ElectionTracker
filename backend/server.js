1fbb5dc93fecd536873be9413b17d6e2d1fc77c3
Erik <180784081+erhetrada@users.noreply.github.com>
2025-06-22 19:14:30 -0400

    Join to nc_precincts table and respond with precinct_code instead of precinct_id

--- backend/server.js
+++ backend/server.js
@@ -329,12 +329,13 @@ app.get('/:state/election-day-votes', (req, res) => {
   const offset = parseInt(req.query.offset) || 0;
 
   db.all(
-    `SELECT vote.vote_id, election.election_date, contest.contest_name, county.county_name, vote.precinct_id, candidate.name, vote.method, vote.vote_count 
+    `SELECT vote.vote_id, election.election_date, contest.contest_name, county.county_name, precinct.precinct_code, candidate.name, vote.method, vote.vote_count 
      FROM nc_voting_methods vote
      JOIN nc_candidates candidate ON vote.candidate_id = candidate.candidate_id
      JOIN nc_counties county on vote.county_id = county.county_id 
      JOIN nc_contests contest on vote.contest_id = contest.contest_id 
      JOIN nc_elections election on vote.election_id = election.election_id 
+     JOIN nc_precincts precinct on vote.precinct_id = precinct.precinct_id 
      WHERE method = ? 
      LIMIT ? OFFSET ?`,
     ['Election Day', limit, offset],
